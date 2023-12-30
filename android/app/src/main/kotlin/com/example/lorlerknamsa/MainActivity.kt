053e7f1b6d3b8985de79fd933e17c962c65afb43
QILIN <sengheng.measheanh@gmail.com>
2023-12-30 21:51:42 +0700

    build auth screen and set up Firebase SDK

---
+++ android/app/src/main/kotlin/com/example/lorlerknamsa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lorlerknamsa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:lorlerknamsa/screens/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterChat',
+      theme: ThemeData().copyWith(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 17, 174, 177)),
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
