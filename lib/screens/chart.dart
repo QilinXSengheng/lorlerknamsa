13839bfd4216f8e349189628374463722404f5e8
QILIN <sengheng.measheanh@gmail.com>
2023-12-30 23:24:34 +0700

    create log out function

---
+++ lib/screens/chart.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class ChartScreen extends StatelessWidget {
+  const ChartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterChat'),
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.exit_to_app,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+            )
+          ],
+        ),
+        body: const Center(
+          child: Text('Logged In'),
+        ));
+  }
+}
